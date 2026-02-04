1e567b5b06d0cf939dc9737e309b7610277e4c48
yunjindev <hunterweisenbach@me.com>
2026-02-04 12:51:48 -0500

    string slices

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// PAGE NUMBER: 48/374 -- FEB 3, 2026
+// PAGE NUMBER: 48/374 -- FEB 4, 2026
 // LOCK IN 
 
 package main
@@ -6,8 +6,8 @@ package main
 import ( "fmt" )
 
 func main() {
-	x := []int{1,2,3,4}
-	y := make([]int,4)
-	num := copy(y[:3],x[2:])
-	fmt.Println(y,num)
+	var s string = "Hello There"
+	var s2 string = s[4:7]
+	fmt.Println(s,s2) // Hello There & o th
+
 }
