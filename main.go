0c68798fbedebd03020381427f4ab27611a18bb6
yunjindev <hunterweisenbach@me.com>
2026-02-04 00:37:03 -0500

    page 48

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// PAGE NUMBER: 44/374 -- FEB 3, 2026
+// PAGE NUMBER: 48/374 -- FEB 3, 2026
 // LOCK IN 
 
 package main
@@ -6,16 +6,8 @@ package main
 import ( "fmt" )
 
 func main() {
-	x := make([]int,0,8) // 0 ints, 5 cap
-	x = append(x, 1,2,3,4)
-	y:= x[:2] // 1 2
-	z := x[2:] // 3 4
-	fmt.Println(cap(x), cap(y), cap(z)) //5 5 3
-	y = append(y, 30,40,50)
-	x = append(x,60)
-	z = append(z,70)
-	fmt.Println(x)
-	fmt.Println(y)
-	fmt.Println(z)
-
+	x := []int{1,2,3,4}
+	y := make([]int,4)
+	num := copy(y[:3],x[2:])
+	fmt.Println(y,num)
 }
