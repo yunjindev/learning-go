f1ab962ed2ad807f131a4a95cbafceed098ab3d6
yunjindev <hunterweisenbach@me.com>
2026-02-05 15:30:32 -0500

    page 58

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
-// PAGE NUMBER: 53/374 -- FEB 4, 2026
+// PAGE NUMBER: 58/374 -- FEB 4, 2026
 // LOCK IN 
 
 package main
 
-import ( "fmt" )
 
 func main() {
-	nilMap := map[string][]int {
-		"Dan" : []int{2,3,4,5},
-		"Billy" : []int{5,42,1,6,7},
-		"James" : []int{4},
-	}
-	fmt.Println(nilMap)
+	 type person struct {
+		 name string
+		 age int
+		 pet string
+	 }
+	 var fred person
+	 bob := person{}
+	 julia := person{
+		 "julia",
+		 40,
+		 "cat",
+	 }
+	 beth := person{
+		 age: 30,
+		 name: "bethany",
+	 }
 }
