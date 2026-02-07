e41da2e81bfccebdb02ea985b697dc7d68bd7431
yunjindev <hunterweisenbach@me.com>
2026-02-06 21:48:37 -0500

    fizzbuzz

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
-// PAGE NUMBER: 61/374 -- FEB 4, 2026
+// PAGE NUMBER: 71/374 -- FEB 4, 2026
 // LOCK IN 
 
 package main
 
-import "fmt"
+import ( 
+	"fmt"
 
-// TODO find out what the type is in a struct
+)
+
 func main() {
-	type firstPerson struct {
-		name string
-		age int
-}
+	for i := 1; i < 101; i++ {
+		if i % 3 == 0 && i % 5 == 0 {
+			fmt.Println("FizzBuzz ", i)
+		}
+		if i % 5 == 0 {
+			fmt.Println("Buzz", i)
+		}
+		if i % 3 == 0 {
+			fmt.Println("Fizz", i)
+			}
+		fmt.Println(i)
+		}	
 
-	f := firstPerson {
-		name: "bob",
-		age: 50,
 	}
 
-	var g struct {
-		name string
-		ages int
-	}
-	g = f
-	fmt.Println(f == g)
-}
