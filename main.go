843523952f1593a225b4207f0eaa87e1d5995074
yunjindev <hunterweisenbach@me.com>
2026-02-08 22:50:45 -0500

    page 88

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// PAGE NUMBER: 76/374 -- FEB 4, 2026
+// PAGE NUMBER: 88/374 -- FEB 4, 2026
 // LOCK IN 
 
 package main
@@ -7,18 +7,4 @@ import (
 	"fmt"
 )
 
-func main() {
-	a := 2
-	switch 	{
-	case a == 2:
-		fmt.Println("a is 2")
-	case a == 3:
-		fmt.Println("a is 3")
-	case a == 4:
-		fmt.Println("a is 4")
-	default:
-		fmt.Println("a is ", a)
-
-	}
-}
 
