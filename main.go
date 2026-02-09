854fc5004844d6d4b0d8b980ba2daf41056d32ce
yunjindev <hunterweisenbach@me.com>
2026-02-09 12:20:00 -0500

    page 90

--- main.go
+++ main.go
@@ -3,8 +3,15 @@
 
 package main
 
-import ( 
-	"fmt"
-)
+import "fmt"
 
-
+func addTo(base int, vals ...int) []int {
+	returnOut := make([]int, 0, len(vals))
+	for _, v := range vals {
+		returnOut = append(returnOut, base+v)
+	}
+	return returnOut
+}
+func main() {
+	fmt.Println(addTo(3,3,4,3))
+}
