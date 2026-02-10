24999d0045d892d8802a3f8855cf74ba39053569
yunjindev <hunterweisenbach@me.com>
2026-02-10 11:50:58 -0500

    page 101

--- main.go
+++ main.go
@@ -1,26 +1,21 @@
-// PAGE NUMBER: 90/374 -- FEB 4, 2026
+// PAGE NUMBER: 101/374 -- FEB 10, 2026
 // LOCK IN 
 
 package main
 
 import ( 
 	"fmt"
-	"errors"
-	"os"
 )
-
-func divAndRemainder(num int, denom int) (int,int,error) {
-	if denom == 0 {
-		return 0,0, errors.New("cannot divide by 0")
+func makeMult(base int) func(int) int {
+	return func(factor int) int {
+		return base * factor
 	}
-	return num / denom, num % denom, nil
 }
 
 func main() {
-	result, remainder, err := divAndRemainder(_,_)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
+	twoBase := makeMult(5)
+	threeBase := makeMult(3)
+	for i := 0; i < 3; i++ {
+		fmt.Println(twoBase(i), threeBase(i))
 	}
-	fmt.Println(result, remainder)
 }
