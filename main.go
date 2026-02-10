1c3bd0295fc67b1296820c7451d639272104e0f1
yunjindev <hunterweisenbach@me.com>
2026-02-09 22:06:23 -0500

    page 92

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
-// PAGE NUMBER: 88/374 -- FEB 4, 2026
+// PAGE NUMBER: 90/374 -- FEB 4, 2026
 // LOCK IN 
 
 package main
 
-import "fmt"
+import ( 
+	"fmt"
+	"errors"
+	"os"
+)
 
-func addTo(base int, vals ...int) []int {
-	returnOut := make([]int, 0, len(vals))
-	for _, v := range vals {
-		returnOut = append(returnOut, base+v)
+func divAndRemainder(num int, denom int) (int,int,error) {
+	if denom == 0 {
+		return 0,0, errors.New("cannot divide by 0")
 	}
-	return returnOut
+	return num / denom, num % denom, nil
 }
+
 func main() {
-	fmt.Println(addTo(3,3,4,3))
+	result, remainder, err := divAndRemainder(_,_)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	fmt.Println(result, remainder)
 }
