00010bdb730c2f482cbe76928eea84e77c1b5467
yunjindev <hunterweisenbach@me.com>
2026-02-11 16:28:10 -0500

    Page 107 added

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// PAGE NUMBER: 101/374 -- FEB 10, 2026
+// PAGE NUMBER: 107/374 -- FEB 10, 2026
 // LOCK IN 
 
 package main
@@ -6,16 +6,10 @@ package main
 import ( 
 	"fmt"
 )
-func makeMult(base int) func(int) int {
-	return func(factor int) int {
-		return base * factor
+func main() {
+	m := []int{0,2,3,4}
+	for k, v := range m {
+		fmt.Println(k,v,m)
 	}
 }
 
-func main() {
-	twoBase := makeMult(5)
-	threeBase := makeMult(3)
-	for i := 0; i < 3; i++ {
-		fmt.Println(twoBase(i), threeBase(i))
-	}
-}
